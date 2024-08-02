9220595eb170fef6b9112c984d4b701dd2cd9b64
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-08-02 16:24:57 -0400

    Add YouTube API integration to fetch most popular videos
    - Implement YouTube API client using googleapiclient.discovery
    - Fetch most popular videos in the US region
    - Print the response in JSON format

--- .venv/main.py
+++ .venv/main.py
@@ -1,10 +1,12 @@
 import json
-import os
-import numpy as np
-import pandas as pd
+from secrets import api_key
+
 import googleapiclient.discovery
 import googleapiclient.errors
 
-apiKey=os.environ["API_KEY"]
-
-
+apiServiceName = "youtube"
+apiVersion = "v3"
+youtube = googleapiclient.discovery.build(apiServiceName, apiVersion, developerKey = api_key)
+request = youtube.videos().list(part="snippet,contentDetails,statistics", chart="mostPopular", regionCode= "US")
+response = request.execute()
+print(json.dumps(response, indent=2))
