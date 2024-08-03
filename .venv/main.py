956e188dd8a4fd1af2693501527c390465225e9d
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-08-03 15:33:37 -0400

    Refactor YouTube API integration and add data processing
    - Refactor  function to handle pagination for more than 50 results
    - Fetch up to 1000 most popular videos in the US region
    - Normalize JSON response into a pandas DataFrame
    - Filter and rename relevant columns
    - Calculate and add 'Like Ratio' and 'Comment Ratio' columns
    - Export the processed data to

--- .venv/main.py
+++ .venv/main.py
@@ -4,10 +4,35 @@ import pandas as pd
 import numpy as np
 import googleapiclient.discovery
 import googleapiclient.errors
-
+pd.options.mode.chained_assignment = None
 apiServiceName = "youtube"
 apiVersion = "v3"
-youtube = googleapiclient.discovery.build(apiServiceName, apiVersion, developerKey = api_key)
-request = youtube.videos().list(part="snippet,contentDetails,statistics", chart="mostPopular", regionCode= "US")
-response = request.execute()
-print(json.dumps(response, indent=2))
+def getYoutubeVideos(api_key, max_results):
+    youtube = googleapiclient.discovery.build(apiServiceName, apiVersion, developerKey = api_key)
+    request = youtube.videos().list(part="snippet,contentDetails,statistics", chart="mostPopular", regionCode= "US", maxResults=max_results)
+
+    all_videos =[]
+    while request is not None:
+        response = request.execute()
+        all_videos.extend(response['items'])
+        request = youtube.videos().list_next(request, response)
+    return all_videos
+
+videos = getYoutubeVideos(api_key, 1000)
+print(len(videos))
+
+video_df = pd.json_normalize(videos)
+video_df = video_df.fillna(0)
+filtered_df = video_df[['snippet.title', 'snippet.channelTitle', 'snippet.publishedAt', 'statistics.viewCount', 'statistics.likeCount', 'statistics.commentCount']]
+filtered_df.columns = ['Title', 'Channel', 'Published At', 'View Count', 'Like Count', 'Comment Count']
+filtered_df['Published At'] = pd.to_datetime(filtered_df['Published At'])
+filtered_df['Published At'] = filtered_df['Published At'].dt.date
+filtered_df['View Count'] = filtered_df['View Count'].astype(int)
+filtered_df['Like Count'] = filtered_df['Like Count'].astype(int)
+filtered_df['Comment Count'] = filtered_df['Comment Count'].astype(int)
+filtered_df['Like Ratio'] = filtered_df['Like Count'] / filtered_df['View Count']
+filtered_df['Comment Ratio'] = filtered_df['Comment Count'] / filtered_df['View Count']
+filtered_df.to_csv('video_data.csv', index=True)
+
+
+
